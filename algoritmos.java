8244d0dfba29ae95433b171e713efeaf133bdfdf
MP Lab04 PC27 <mplab4.pc27@cloud.uautonoma.cl>
2024-05-09 09:24:16 -0400

    avance 1

--- algoritmos.java
+++ algoritmos.java
@@ -1,16 +1,31 @@
 public class algoritmos {
     public static void BubleSort(int[] arr){
         int fin = arr.length;
+        int aux;
         for (int i = 0; i<arr.length; i++){
             for (int j = 0; j<fin-1; j++){
                 if (arr[j] > arr[j+1]){
-                    int aux = arr[j];
+                    aux = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = aux;
                     Pantalla.cambioVar(aux, arr[j],j+1,j+2);
                 }
             }
             fin--;
+        }
+        Pantalla.mostrarArr(arr);
+    }
+
+    public static void SelectionSort(int[] arr){
+        int aux;
+        for (int i = 1; i<arr.length; i++){
+            if (arr[i]<arr[i-1]){
+                for (int j = i; j>0; j--){
+                    aux = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = aux;
+                }
+            }
         }
         Pantalla.mostrarArr(arr);
     }
--- main.java
+++ main.java
@@ -1,13 +1,15 @@
 import java.util.Random;
 
 public class main{
-    public static void main(String[] args) {
-        int[] arr = new int[20];
+    public static void main(String[] _args) {
+        int largo = 5;
+        int[] arr = new int[largo];
         Random ran = new Random();
         for (int i = 0; i<arr.length;i++){
             arr[i] = ran.nextInt(100);
         }
         Pantalla.mostrarArr(arr);
-        algoritmos.BubleSort(arr);
+        algoritmos.BubleSort(arr.clone());
+        algoritmos.SelectionSort(arr.clone());
     }
 }
